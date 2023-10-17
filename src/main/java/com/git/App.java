5bc6717ec1a30b1fac11c987942d5df915033f07
ouzammad <yassineouzammad17@gmail.com>
2023-10-17 09:29:00 +0200

    correction;

--- src/main/java/com/git/App.java
+++ src/main/java/com/git/App.java
@@ -3,7 +3,8 @@ package com.git;
 public class App {
     
     public static void main(String[] args) {
-        System.out.println("hello world!")
+
+        System.out.println("hello world!");
     }
     
 }
